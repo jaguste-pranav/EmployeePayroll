4424901da9980bac148287839b5c73910d20ae42
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-11-10 02:16:15 +0530

    [pranav] Add. Update the employee details

--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -2,7 +2,7 @@
 
 namespace EmployeePayrollService
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -38,6 +38,51 @@ namespace EmployeePayrollService
             {
                 Console.WriteLine("Employee isn't Added");
             }
+
+        }
+    }
+}
+using System;
+
+namespace EmployeePayrollService
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Service!");
+
+            EmployeeModel model = new EmployeeModel();
+            model.EmployeeName = "Akash";
+            model.PhoneNumber = "9875241630";
+            model.Address = "Mumbai";
+            model.Department = "SD";
+            model.Gender = 'M';
+            model.BasicPay = 53000;
+            model.Deductions = 2000;
+            model.TaxablePay = 500;
+            model.Tax = 1000;
+            model.NetPay = 50000;
+            model.StartDate = DateTime.Now;
+            model.City = "Mulund";
+            model.Country = "In";
+
+            EmployeeRepo repo = new EmployeeRepo();
+
+            Console.WriteLine("Displaying All Data");
+            repo.GetAllEmployee();
+
+            bool res = repo.AddEmployee(model);
+            if (res)
+            {
+                Console.WriteLine("Employee Added Successfully");
+                repo.GetAllEmployee();
+            }
+            else
+            {
+                Console.WriteLine("Employee isn't Added");
+            }
+
         }
     }
 }
